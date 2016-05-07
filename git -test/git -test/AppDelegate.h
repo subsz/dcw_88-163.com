0f88353cc721553f1fecd8e9fb99d535f257e9ce
邓超文 <dengchaowen@six-ladytaobaocom.local>
2016-05-07 19:58:33 +0800

    这是第一次提交代码

---
+++ git -test/git -test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git -test
+//
+//  Created by 邓超文 on 16/5/7.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
